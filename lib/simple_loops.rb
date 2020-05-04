f52f2ae005bc6ad4e4a63a4e55af16609be55f8d
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 20:05:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,6 +9,11 @@ def loop_message_five_times(message)
 end
 
 def loop_message_n_times(message, n)
+  count = 0 
+  while count < n+1 
+  puts message
+  count += 1 
+end
 end
 
 def output_array(message, n)
