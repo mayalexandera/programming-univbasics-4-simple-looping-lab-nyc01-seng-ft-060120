f39cbb6cb35551b7ae6ae87b65e3a2106b513c12
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 20:06:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,19 +1,19 @@
 # Write your methods here
 
 def loop_message_five_times(message)
-   count = 0
-   while count < 6
-   puts message
-   count += 1
- end
+  count = 0
+  while count < 6
+    puts message
+    count += 1
+  end
 end
 
 def loop_message_n_times(message, n)
   count = 0 
   while count < n+1 
-  puts message
-  count += 1 
-end
+    puts message
+    count += 1 
+  end
 end
 
 def output_array(message, n)
