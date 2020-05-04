57272a03c7a34ebe75cd2304166013b68b9ccbb8
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 20:04:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  5.times do 
+    puts message 
+end
+
+def loop_message_n_times(message, n)
+end
+
+def output_array(message, n)
+end
+
+def return_string_array(array)
+end
